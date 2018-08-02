0695425ea84cf5801c486c49dadecd5e6cebb5a9
Torok Mark <torokmark@gmx.com>
2018-08-02 16:09:46 +0200

    Add helper class

--- lib/encrubto/rot13/helper.rb
+++ lib/encrubto/rot13/helper.rb
@@ -3,7 +3,19 @@ module Encrubto::Rot13
   module Helper
   
     def self.encrypt(str)
-     puts 'hello' 
+      if str.is_a? String
+        str.chars.map { |c|
+          if ('a'..'z').include? c
+            
+          elsif ('A'..'Z').include? c
+            
+          else
+            c
+          end
+        }
+      else
+        raise 'Param must be String!'
+      end
     end
 
     def self.decrypt(encrypted)
