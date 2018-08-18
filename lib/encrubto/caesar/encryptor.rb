659d3115d322dea5c52390dcdca32e68369ebab3
Noemi Balogh <szuromberek@gmail.com>
2018-08-18 18:29:52 +0200

    added caesar encryption and tests

---
+++ lib/encrubto/caesar/encryptor.rb
@@ -0,0 +1,34 @@
+
+module Encrubto::Caesar
+    class Encryptor 
+    
+      ORIGINAL = 'abcdefghijklmnopqrstuvwxyz'
+
+      def encrypt(str, offset)
+        if ( (str.is_a? String) && (offset.is_a? Integer) )
+          caesar = shift(offset)
+          str.tr("#{ ORIGINAL }#{ ORIGINAL.upcase }", "#{ caesar }#{ caesar.to_s.upcase }")
+        else
+          raise 'First param must be String, second param must be Integer!'
+        end
+      end
+  
+      def decrypt(encrypted, offset)
+        if ( (encrypted.is_a? String) && (offset.is_a? Integer) )
+            caesar = shift(offset)
+          encrypted.tr("#{ caesar }#{ caesar.to_s.upcase }", "#{ ORIGINAL }#{ ORIGINAL.upcase }")
+        else
+            raise 'First param must be String, second param must be Integer!'
+        end
+      end
+
+      def shift(offset)
+        caesarFirstPart = ORIGINAL[offset..ORIGINAL.length-1]
+        caesarSecondPart = ORIGINAL[0..offset-1]
+        caesar = caesarFirstPart.to_s + caesarSecondPart.to_s
+        return caesar
+      end
+
+    end
+  end
+  
---
+++ spec/caesar/caesar_spec.rb
@@ -0,0 +1,63 @@
+RSpec.describe Encrubto::Caesar do
+
+    describe 'call encrypt' do
+      it 'returns B if input is A' do
+        expect(Encrubto::Caesar.encrypt('A', 1)).to eq('B')
+      end
+
+      it 'returns N if input is A' do
+        expect(Encrubto::Caesar.encrypt('A', 13)).to eq('N')
+      end
+  
+      it 'returns nCcYr if input is aPpLe' do
+        expect(Encrubto::Caesar.encrypt('aPpLe', 13)).to eq('nCcYr')
+      end
+  
+      it 'raises error if number of params is zero' do
+        expect { Encrubto::Caesar.encrypt }.to raise_error(ArgumentError)
+      end
+
+      it 'raises error if number of params is one' do
+        expect { Encrubto::Caesar.encrypt("A") }.to raise_error(ArgumentError)
+      end
+  
+      it 'raises error if first param is not string' do
+        expect { Encrubto::Caesar.encrypt(1, 10) }.to raise_exception('First param must be String, second param must be Integer!')
+      end
+
+      it 'raises error if second param is not integer' do
+        expect { Encrubto::Caesar.encrypt("A", "B") }.to raise_exception('First param must be String, second param must be Integer!')
+      end
+    end
+  
+    describe 'call decrypt' do
+      it 'returns A if input is B' do
+        expect(Encrubto::Caesar.decrypt('B', 1)).to eq('A')
+      end
+
+      it 'returns A if input is N' do
+        expect(Encrubto::Caesar.decrypt('N', 13)).to eq('A')
+      end
+  
+      it 'returns aPpLe if input is nCcYr' do
+        expect(Encrubto::Caesar.decrypt('nCcYr', 13)).to eq('aPpLe')
+      end
+  
+      it 'raises error if number of params is zero' do
+        expect { Encrubto::Caesar.encrypt }.to raise_error(ArgumentError)
+      end
+
+      it 'raises error if number of params is one' do
+        expect { Encrubto::Caesar.encrypt("A") }.to raise_error(ArgumentError)
+      end
+  
+      it 'raises error if first param is not string' do
+        expect { Encrubto::Caesar.encrypt(1, 10) }.to raise_exception('First param must be String, second param must be Integer!')
+      end
+
+      it 'raises error if second param is not integer' do
+        expect { Encrubto::Caesar.encrypt("A", "B") }.to raise_exception('First param must be String, second param must be Integer!')
+      end
+    end
+  end
+  
