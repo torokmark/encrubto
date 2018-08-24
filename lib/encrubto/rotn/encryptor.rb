46b9207e635de7bb1dc8e7bb64292fdeb062b64f
Noemi Balogh <szuromberek@gmail.com>
2018-08-24 20:52:11 +0200

    removed parentheses from if statement and added tests for negative offset

--- lib/encrubto/rotn/encryptor.rb
+++ lib/encrubto/rotn/encryptor.rb
@@ -4,7 +4,7 @@ module Encrubto::RotN
     ORIGINAL = 'abcdefghijklmnopqrstuvwxyz'
 
     def encrypt(str, offset)
-      if (str.is_a?(String) && offset.is_a?(Integer))
+      if str.is_a?(String) && offset.is_a?(Integer)
           rotn = shift(offset % 26)
           str.tr("#{ ORIGINAL }#{ ORIGINAL.upcase }", "#{ rotn }#{ rotn.to_s.upcase }")
       else
@@ -13,7 +13,7 @@ module Encrubto::RotN
     end
 
     def decrypt(encrypted, offset)
-      if (encrypted.is_a?(String) && offset.is_a?(Integer))
+      if encrypted.is_a?(String) && offset.is_a?(Integer)
         rotn = shift(offset % 26)
         encrypted.tr("#{ rotn }#{ rotn.to_s.upcase }", "#{ ORIGINAL }#{ ORIGINAL.upcase }")
       else
--- spec/rotn/rotn_spec.rb
+++ spec/rotn/rotn_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe Encrubto::RotN do
       expect(Encrubto::RotN.encrypt('aPpLe', 39)).to eq('nCcYr')
     end
 
+    it 'returns Z if input is A and -1' do
+      expect(Encrubto::RotN.encrypt('A', -1)).to eq('Z')
+    end
+
+    it 'returns Z if input is A and -27' do
+      expect(Encrubto::RotN.encrypt('A', -27)).to eq('Z')
+    end
+
     it 'raises error if number of params is zero' do
       expect { Encrubto::RotN.encrypt }.to raise_error(ArgumentError)
     end
@@ -41,6 +49,14 @@ RSpec.describe Encrubto::RotN do
 
     it 'returns aPpLe if input is nCcYr and 39' do
       expect(Encrubto::RotN.decrypt('nCcYr', 39)).to eq('aPpLe')
+    end
+
+    it 'returns A if input is Z and -1' do
+      expect(Encrubto::RotN.decrypt('Z', -1)).to eq('A')
+    end
+
+    it 'returns A if input is Z and -27' do
+      expect(Encrubto::RotN.decrypt('Z', -27)).to eq('A')
     end
 
     it 'raises error if number of params is zero' do
