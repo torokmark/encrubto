c3a2a1986b3f7d4c98ffec5cd6dafe37d8930fdd
Noemi Balogh <szuromberek@gmail.com>
2018-10-06 19:03:29 +0200

    Add base64 encoder

--- lib/encrubto/base64/encryptor.rb
+++ lib/encrubto/base64/encryptor.rb
@@ -1,14 +1,46 @@
 module Encrubto::Base64
   class Encryptor
 
+    BASE64 = [
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+    ]
+
     def encrypt(str)
+      result = ""
       byte_array = str.each_byte.to_a
-      $i = 0;
-      integer = Integer(byteArray[0] && 0xff << 16)
+      slices = byte_array.each_slice(3).to_a
+      slices[0..-2].each {
+        |slice|
+        result = result + to_base64(slice)
+      }
+      if slices[-1].length == 3
+        result = result + to_base64(slices[-1])
+      elsif slices[-1].length == 1
+        bit0 = slices[-1][0] & 0xff
+        result = result + BASE64[bit0 >> 2]
+        result = result + BASE64[(bit0 << 4) & 0x3f]
+        result = result + "=="
+      elsif slices[-1].length == 2
+        bit0 = slices[-1][0] & 0xff
+        result = result + BASE64[bit0 >> 2]
+        bit1 = slices[-1][1] & 0xff;
+        result = result + BASE64[(bit0 << 4) & 0x3f | (bit1 >> 4)]
+        result = result + BASE64[(bit1 << 2) & 0x3f]
+        result = result + "="
+      end
+      return result
     end
 
-    def code_table(byte)
-
+    def to_base64(slice)
+      three_bytes = ((slice[0]) << 16) | ((slice[1]) << 8) | (slice[2] & 0xff)
+      four_chars = BASE64[(three_bytes >> 18) & 0x3f] + 
+        BASE64[(three_bytes >> 12) & 0x3f] + 
+        BASE64[(three_bytes >> 6) & 0x3f] + 
+        BASE64[three_bytes & 0x3f]
     end
 
   end
--- spec/base64/base64_spec.rb
+++ spec/base64/base64_spec.rb
@@ -1,8 +1,20 @@
 RSpec.describe Encrubto::Base64 do
 
   describe 'call encrypt' do
-    it 'returns 97 if input is ab' do
-      expect(Encrubto::Base64.encrypt('ab')).to eq(97)
+    it 'returns YWFh if input is aaa' do
+      expect(Encrubto::Base64.encrypt('aaa')).to eq('YWFh')
+    end
+    it 'returns YWFhYWFh if input is aaaaaa' do
+      expect(Encrubto::Base64.encrypt('aaaaaa')).to eq('YWFhYWFh')
+    end
+    it 'returns YWFhYWE= if input is aaaaa' do
+      expect(Encrubto::Base64.encrypt('aaaaa')).to eq('YWFhYWE=')
+    end
+    it 'returns "YmFzZTY0IHRlc3QgdGV4dA==" if input is "base64 test text"' do
+      expect(Encrubto::Base64.encrypt('base64 test text')).to eq('YmFzZTY0IHRlc3QgdGV4dA==')
+    end
+    it 'returns "MTIz" if input is "123"' do
+      expect(Encrubto::Base64.encrypt('123')).to eq('MTIz')
     end
   end
 
