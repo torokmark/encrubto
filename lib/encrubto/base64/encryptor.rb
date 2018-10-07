b2af60c727c02e6e947648482df9a91fc78928c1
Noemi Balogh <szuromberek@gmail.com>
2018-10-07 23:09:29 +0200

    WIP: decoder

--- lib/encrubto/base64/encryptor.rb
+++ lib/encrubto/base64/encryptor.rb
@@ -22,25 +22,51 @@ module Encrubto::Base64
       elsif slices[-1].length == 1
         bit0 = slices[-1][0] & 0xff
         result = result + BASE64[bit0 >> 2]
-        result = result + BASE64[(bit0 << 4) & 0x3f]
+        result = result + BASE64[bit0 << 4 & 0x3f]
         result = result + "=="
       elsif slices[-1].length == 2
         bit0 = slices[-1][0] & 0xff
         result = result + BASE64[bit0 >> 2]
         bit1 = slices[-1][1] & 0xff;
-        result = result + BASE64[(bit0 << 4) & 0x3f | (bit1 >> 4)]
-        result = result + BASE64[(bit1 << 2) & 0x3f]
+        result = result + BASE64[bit0 << 4 & 0x3f | bit1 >> 4]
+        result = result + BASE64[bit1 << 2 & 0x3f]
         result = result + "="
       end
       return result
     end
 
     def to_base64(slice)
-      three_bytes = ((slice[0]) << 16) | ((slice[1]) << 8) | (slice[2] & 0xff)
-      four_chars = BASE64[(three_bytes >> 18) & 0x3f] + 
-        BASE64[(three_bytes >> 12) & 0x3f] + 
-        BASE64[(three_bytes >> 6) & 0x3f] + 
+      three_bytes = slice[0] << 16 | slice[1] << 8 | slice[2]
+      
+      print((three_bytes).to_s(2))
+        print(" ")
+        print((three_bytes >> 18).to_s(2))
+        print(" ")
+        print((three_bytes >> 12 & 0x3f).to_s(2))
+          print(" ")
+      print((three_bytes >> 6 & 0x3f).to_s(2))
+        print(" ")
+        print((three_bytes & 0x3f).to_s(2))
+
+      four_chars = BASE64[three_bytes >> 18 & 0x3f] + 
+        BASE64[three_bytes >> 12 & 0x3f] + 
+        BASE64[three_bytes >> 6 & 0x3f] + 
         BASE64[three_bytes & 0x3f]
+    end
+
+    def decrypt(str)
+      result = ""
+      byte_array = str.each_byte.to_a
+      slices = byte_array.each_slice(4).to_a
+      slices.each {
+        |slice|
+        result = from_base64(slice)
+      }
+      return result
+    end
+
+    def from_base64(slice)
+      integer = (BASE64.find_index(slice[0].chr) & 0xff).to_s(2)
     end
 
   end
--- spec/base64/base64_spec.rb
+++ spec/base64/base64_spec.rb
@@ -10,12 +10,24 @@ RSpec.describe Encrubto::Base64 do
     it 'returns YWFhYWE= if input is aaaaa' do
       expect(Encrubto::Base64.encrypt('aaaaa')).to eq('YWFhYWE=')
     end
+    it 'returns YWFhYQ== if input is aaaa' do
+      expect(Encrubto::Base64.encrypt('aaaa')).to eq('YWFhYQ==')
+    end
     it 'returns "YmFzZTY0IHRlc3QgdGV4dA==" if input is "base64 test text"' do
       expect(Encrubto::Base64.encrypt('base64 test text')).to eq('YmFzZTY0IHRlc3QgdGV4dA==')
     end
     it 'returns "MTIz" if input is "123"' do
       expect(Encrubto::Base64.encrypt('123')).to eq('MTIz')
     end
+    it 'returns "PQ==" if input is "="' do
+      expect(Encrubto::Base64.encrypt('=')).to eq('PQ==')
+    end
   end
+
+  describe 'call decrypt' do
+    it 'returns aaa if input is YWFh' do
+     expect(Encrubto::Base64.decrypt('YWFh')).to eq('aaa')
+    end
+ end
 
 end
