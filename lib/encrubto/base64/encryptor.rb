7a202250c6680009aa381ad576940cebb4f5f994
Noemi Balogh <szuromberek@gmail.com>
2018-10-13 10:10:38 +0200

    Refactor base64

--- lib/encrubto/base64/encryptor.rb
+++ lib/encrubto/base64/encryptor.rb
@@ -13,66 +13,66 @@ module Encrubto::Base64
       result = ""
       byte_array = str.each_byte.to_a
       slices = byte_array.each_slice(3).to_a
-      slices[0..-2].each {
-        |slice|
+      slices[0..-2].each { |slice|
         result = result + to_base64(slice)
       }
-      if slices[-1].length == 3
-        result = result + to_base64(slices[-1])
-      elsif slices[-1].length == 1
-        bit0 = slices[-1][0] & 0xff
-        result = result + BASE64[bit0 >> 2]
-        result = result + BASE64[bit0 << 4 & 0x3f]
-        result = result + "=="
-      elsif slices[-1].length == 2
-        bit0 = slices[-1][0] & 0xff
-        result = result + BASE64[bit0 >> 2]
-        bit1 = slices[-1][1] & 0xff;
-        result = result + BASE64[bit0 << 4 & 0x3f | bit1 >> 4]
-        result = result + BASE64[bit1 << 2 & 0x3f]
-        result = result + "="
-      end
-      return result
-    end
-
-    def to_base64(slice)
-      bits = slice[0] << 16 | slice[1] << 8 | slice[2]
-      chars = BASE64[bits >> 18 & 0x3f] + 
-        BASE64[bits >> 12 & 0x3f] + 
-        BASE64[bits >> 6 & 0x3f] + 
-        BASE64[bits & 0x3f]
+      result = result + to_base64_last_slice(slices[-1])
     end
 
     def decrypt(str)
       result = ""
       byte_array = str.each_byte.to_a
       slices = byte_array.each_slice(4).to_a
-      slices.each {
-        |slice|
+      slices.each { |slice|
         result = result + from_base64(slice)
       }
       return result
     end
 
-    def from_base64(slice)
-      array = []
-      slice.each {
-        |s| 
-        if s.chr != "="
-          array.push(BASE64.find_index(s.chr))
+    def to_base64(byte_array)
+      bits = byte_array[0] << 16 | byte_array[1] << 8 | byte_array[2]
+      chars = BASE64[bits >> 18 & 0x3f] + 
+        BASE64[bits >> 12 & 0x3f] + 
+        BASE64[bits >> 6 & 0x3f] + 
+        BASE64[bits & 0x3f]
+    end
+
+    def to_base64_last_slice(byte_array)
+      chars = ""
+      if byte_array.length == 3
+        chars = chars + to_base64(byte_array)
+      elsif byte_array.length == 1
+        bit0 = byte_array[0] & 0xff
+        chars = chars + BASE64[bit0 >> 2]
+        chars = chars + BASE64[bit0 << 4 & 0x3f]
+        chars = chars + "=="
+      elsif byte_array.length == 2
+        bit0 = byte_array[0] & 0xff
+        chars = chars + BASE64[bit0 >> 2]
+        bit1 = byte_array[1] & 0xff;
+        chars = chars + BASE64[bit0 << 4 & 0x3f | bit1 >> 4]
+        chars = chars + BASE64[bit1 << 2 & 0x3f]
+        chars = chars + "="
+      end
+    end
+
+    def from_base64(base64_chars)
+      base64_indexes = []
+      base64_chars.each { |char|
+        if char.chr != "="
+          base64_indexes.push(BASE64.find_index(char.chr))
         else
-          array.push(s)
+          base64_indexes.push(char)
         end
       }
-      
-      if array[2].chr == "=" && array[3].chr == "="
-        bits = array[0] << 6 | array[1]
+      if base64_indexes[2].chr == "=" && base64_indexes[3].chr == "="
+        bits = base64_indexes[0] << 6 | base64_indexes[1]
         chars = (bits >> 4 & 0xff).chr
-      elsif array[3].chr == "="
-        bits = array[0] << 12 | array[1] << 6 | array[2]
+      elsif base64_indexes[3].chr == "="
+        bits = base64_indexes[0] << 12 | base64_indexes[1] << 6 | base64_indexes[2]
         chars = (bits >> 10 & 0xff).chr + (bits >> 2 & 0xff).chr
       else
-        bits = array[0] << 18 | array[1] << 12 | array[2] << 6 | array[3]
+        bits = base64_indexes[0] << 18 | base64_indexes[1] << 12 | base64_indexes[2] << 6 | base64_indexes[3]
         chars = (bits >> 16 & 0xff).chr + (bits >> 8 & 0xff).chr + (bits & 0xff).chr
       end
     end
