782233e1f07d0f068e406861bb9b121b2b6f4182
Noemi Balogh <szuromberek@gmail.com>
2021-04-23 19:07:39 +0200

    added base64 encryptor and tests

---
+++ spec/base64/base64_spec.rb
@@ -0,0 +1,39 @@
+RSpec.describe Encrubto::Base64 do
+
+  describe 'call encrypt' do
+    it 'returns YWFh if input is aaa' do
+      expect(Encrubto::Base64.encrypt('aaa', false)).to eq('YWFh')
+    end
+    it 'returns YWFhYWE= if input is aaaaa' do
+      expect(Encrubto::Base64.encrypt('aaaaa', false)).to eq('YWFhYWE=')
+    end
+    it 'returns "YmFzZTY0IHRlc3QgdGV4dA==" if input is "base64 test text"' do
+      expect(Encrubto::Base64.encrypt('base64 test text', false)).to eq('YmFzZTY0IHRlc3QgdGV4dA==')
+    end
+    it 'returns "MTIz" if input is "123"' do
+      expect(Encrubto::Base64.encrypt('123', false)).to eq('MTIz')
+    end
+    it 'returns "PQ==" if input is "="' do
+      expect(Encrubto::Base64.encrypt('=', false)).to eq('PQ==')
+    end
+  end
+
+  describe 'call decrypt' do
+    it 'returns aaa if input is YWFh' do
+     expect(Encrubto::Base64.decrypt('YWFh', false)).to eq('aaa')
+    end
+    it 'returns aaaaa if input is YWFhYWE=' do
+      expect(Encrubto::Base64.decrypt('YWFhYWE=', false)).to eq('aaaaa')
+    end
+    it 'returns "base64 test text" if input is "YmFzZTY0IHRlc3QgdGV4dA=="' do
+      expect(Encrubto::Base64.decrypt('YmFzZTY0IHRlc3QgdGV4dA==', false)).to eq('base64 test text')
+    end
+    it 'returns 123 if input is MTIz' do
+      expect(Encrubto::Base64.decrypt('MTIz', false)).to eq('123')
+    end
+    it 'returns = if input is PQ==' do
+      expect(Encrubto::Base64.decrypt('PQ==', false)).to eq('=')
+    end
+ end
+
+end
