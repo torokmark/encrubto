e130e6a95872cd556138de4d9aa8d2058c6b0205
Noemi Balogh <szuromberek@gmail.com>
2021-04-24 17:07:59 +0200

    added documentation

--- lib/encrubto/affine/encryptor.rb
+++ lib/encrubto/affine/encryptor.rb
@@ -4,11 +4,25 @@ module Encrubto::Affine
     ALPHABET_SIZE = 26
     ASCII_LOWER_A = 'a'.ord
     
+    # Encrypts text using the Affine cipher.
+    #
+    # @param plain_string [String] the text to encrypt
+    # @param a [Integer] the first key
+    # @param b [Integer] the second key
+    # @return [String] the encrypted text
+    # @raise [ArgumentError] if the first key is not coprime with the alphabet size
     def encrypt(plain_string, a=5, b=8)
       cipher_table = encrypt_alphabet(a, b)
       cipher_string = encode(plain_string, cipher_table)
     end
     
+    # Decrypts text using the Affine cipher.
+    #
+    # @param plain_string [String] the text to decrypt
+    # @param a [Integer] the first key
+    # @param b [Integer] the second key
+    # @return [String] the decrypted text
+    # @raise [ArgumentError] if the first key is not coprime with the alphabet size
     def decrypt(cipher_string, a=5, b=8)
       cipher_table = decrypt_alphabet(a,b)
       plain_string = encode(cipher_string, cipher_table)
